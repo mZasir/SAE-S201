15b6a923392dc191b09faaff248abf7f131b9db5
inesmrcz <inesmarcisz@gmail.com>
2024-05-16 13:30:07 +0200

    Version 0 de la classe Nombre

---
+++ Partie I/Nombre.java
@@ -0,0 +1,11 @@
+public class Nombre {
+    private int valeurNombre;
+
+    public class Nombre(int nbr){
+        this.valeurNombre=nbr;
+    }
+
+    public class toString(){
+        return ("Le nombre est :" + this.nbr);
+    }
+}
