5ba04aa1fb8da312d2727f09996256e8285a5904
zFhost <159635455+zFhost@users.noreply.github.com>
2024-05-18 21:20:39 +0200

    ajout class Operation

--- Partie I/Nombre.java
+++ Partie I/Nombre.java
@@ -1,8 +1,14 @@
 public class Nombre {
+    /**
+     * Nom du projet : Calculatrice SAE R201
+     * Auteurs : Zasir MOINOOTHEEN et Inès Marcisz
+     * Date de dernière modification : 2024-05-18
+     */
+
     private int valeurNombre;
 
-    public Nombre(int nbr){
-        this.valeurNombre=nbr;
+    public Nombre(int nombre){
+        this.valeurNombre=nombre;
     }
 
     public int Valeur(){
@@ -10,6 +16,6 @@ public class Nombre {
     }
 
     public String toString(){
-        return ("Le nombre est :" + this.nbr);
+        return ("Le nombre est :" + this.valeurNombre);
     }
 }
