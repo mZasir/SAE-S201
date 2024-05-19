0643162a5cc2ebe0d477778d74f7b1326667a95b
zFhost <159635455+zFhost@users.noreply.github.com>
2024-05-19 17:54:39 +0200

    ajout classe soustraction

---
+++ Partie I/Soustraction.java
@@ -0,0 +1,21 @@
+public class Soustraction extends Operation{
+    /**
+     * Nom du projet : Calculatrice SAE R201
+     * Auteurs : Zasir MOINOOTHEEN et Inès MARICSZ
+     * Date de dernière modification : 2024-05-18
+     */
+    
+     public Soustraction(Nombre nombre1, Nombre nombre2){
+        super(nombre1, nombre2);
+     }
+
+     public int valeur(){
+        return this.getOperande1().valeur() - this.getOperande2().valeur();
+     }
+
+     public String toString(){
+        return "(" + this.getOperande1() + "-" + this.getOperande2() + ")" +
+                "=" + this.valeur();
+     }
+
+}
