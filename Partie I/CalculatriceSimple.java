ed7a80e79148f34d80f393df0a8de5a004aa0eac
inesmrcz <inesmarcisz@gmail.com>
2024-05-19 20:14:17 +0200

    Ajout de la classe test

---
+++ Partie I/CalculatriceSimple.java
@@ -0,0 +1,7 @@
+public class CalculatriceSimple {
+    /**
+     * Nom du projet : Calculatrice SAE R201
+     * Auteurs : Zasir MOINOOTHEEN et Inès Marcisz
+     */
+    public static void main (String args[] )
+}
