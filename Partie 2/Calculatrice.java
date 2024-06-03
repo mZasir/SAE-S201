c610b55807f61bb88e314f09b7dbfabfcf324c07
inesmrcz <inesmarcisz@gmail.com>
2024-06-03 21:33:57 +0200

    Ajout du try et du catch

--- Partie 2/Calculatrice.java
+++ Partie 2/Calculatrice.java
@@ -4,6 +4,7 @@ public class Calculatrice {
      * Auteurs : Zasir MOINOOTHEEN et Inès MARCISZ
      */
     public static void main (String args[]){
+        try { 
         Expression huit = new Nombre(8) ;
         Expression onze = new Nombre(11) ;
         Expression cinq = new Nombre(15 ;
@@ -11,17 +12,22 @@ public class Calculatrice {
         Expression m2 = new Multiplication (m, cinq) ;
         System.out.println(m2 + "=" + m2.valeur()); // affiche (8 * 11) * 5 = 440
 
-        Expression zero = new Nombre (0) ;
-        Expression d = new Division (m, zero) ;
-        System.out.println(d + "=" + d.valeur()) ; // erreur 
-
         Expression deux = new Nombre(2) ;
         Expression trois = new Nombre(3) ;
         Expression dixSept = new Nombre(17) ;
         Expression s = new Soustraction(dixSept, deux) ;
         Expression a = new Addition(deux, trois) ;
         Expression d = new Division(s, a) ;
-        System.out.println(d + " = " + d.valeur()) ; // affiche ((17 - 2) / (2 + 3)) = 3
+        System.out.println(d + " = " + d.valeur()) ; // affiche ((17 - 2) / (2 + 3)) = 3 
+
+        Expression zero = new Nombre (0) ;
+        Expression d = new Division (m, zero) ;
+        System.out.println(d + "=" + d.valeur()) ; 
+
+    } catch (Exception e) {
+        System.out.println("Il y a eu une erreur : " + e) ;
+        }
     }
+
     
 }
