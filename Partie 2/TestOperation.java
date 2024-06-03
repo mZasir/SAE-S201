fd9d506c532a4128ad037c10938081d3b9a0c83d
zFhost <159635455+zFhost@users.noreply.github.com>
2024-06-03 21:50:00 +0200

    Try Catch Erreur Division

--- Partie 2/TestOperation.java
+++ Partie 2/TestOperation.java
@@ -7,7 +7,7 @@ public class TestOperation {
         try { 
         Expression huit = new Nombre(8) ;
         Expression onze = new Nombre(11) ;
-        Expression cinq = new Nombre(15 ;
+        Expression cinq = new Nombre(15) ;
         Expression m = new Multiplication(huit, onze) ;
         Expression m2 = new Multiplication (m, cinq) ;
         System.out.println(m2 + "=" + m2.valeur()); // affiche (8 * 11) * 5 = 440
@@ -15,15 +15,21 @@ public class TestOperation {
         Expression deux = new Nombre(2) ;
         Expression trois = new Nombre(3) ;
         Expression dixSept = new Nombre(17) ;
+
+        try{
+            Expression zero = new Nombre (0) ;
+            Expression d = new Division (m, zero) ;
+            System.out.println(d + "=" + d.valeur()) ; 
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+        
         Expression s = new Soustraction(dixSept, deux) ;
         Expression a = new Addition(deux, trois) ;
-        Expression d = new Division(s, a) ;
-        System.out.println(d + " = " + d.valeur()) ; // affiche ((17 - 2) / (2 + 3)) = 3 
+        Expression d2 = new Division(s, a) ;
+        System.out.println(d2 + " = " + d2.valeur()) ; // affiche ((17 - 2) / (2 + 3)) = 3 
 
-        Expression zero = new Nombre (0) ;
-        Expression d = new Division (m, zero) ;
-        System.out.println(d + "=" + d.valeur()) ; 
-
+        
     } catch (Exception e) {
         System.out.println("Il y a eu une erreur : " + e) ;
         }
